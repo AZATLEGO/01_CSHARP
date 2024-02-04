b37ece0dfec715266307ad8286bb733a96dd492d
azat syrtbaev <syrttbaev7@gmail.com>
2024-02-04 14:19:49 +0100

    lesson: task2

--- C_SHARPlesson1/Task2/Program.cs
+++ C_SHARPlesson1/Task2/Program.cs
@@ -2,3 +2,17 @@
 int i2 = 6;
 int sum i1 + i2;
 console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + i1
+console.WriteLine(sum2);
+
+double d1 = 5.55;
+double sum3 = d1 +i1;
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+double product3 = i1 *  d1;
+console.WriteLine(product1);
+console.WriteLine(product2);
+console.WriteLine(product3);
