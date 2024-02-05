c585c00ac45cb7c7fc420bf8464c152f2babe1b6
azat syrtbaev <syrttbaev7@gmail.com>
2024-02-05 19:14:26 +0100

    lesson2:Task1

---
+++ C_SHARPlesson2/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arr = new int[5];
+arr[0];
+arr[1];
+arr[2];
+arr[3];
+arr[4];
+console.writeline(arr[0]);
+console.writeline(arr[1]);
+console.writeline(arr[2]);
+console.writeline(arr[3]);
+console.writeline(arr[4]);
+
+// console.writeline(arr[5]);
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
+
+// arr =[1,2,3,4,5]
