83efe43b75e1b641bc8ecc659b6a50d7eac4da42
azat syrtbaev <syrttbaev7@gmail.com>
2024-02-05 13:58:41 +0100

    lesson1 :Task1

---
+++ C_SHARPlesson1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 7;
+int c = 8;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = c;
+}
+if (e > max)
+{
+    max = e;
+}
+console.WriteLine(max)
