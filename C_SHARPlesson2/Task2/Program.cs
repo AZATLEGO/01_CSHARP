a4869a89a7ae9db8c04c78e936b129ef611601c5
azat syrtbaev <syrttbaev7@gmail.com>
2024-02-06 12:16:25 +0100

    Task2/

---
+++ C_SHARPlesson2/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.length)
+{
+    arr[i] = i + 1;
+    // console.write(arr[i]);
+    // console.write(' ');
+    console.write($"(arr[i])");
+    i = i + 1;
+
+}
---
+++ C_SHARPlesson2/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// int n = 10;
+int[] array =
+{
+    2, 5, 4, 7, 8, 35, 1,13, 8, 5
+};
+int i = 0;
+
+while (i < array.lenth)
+{
+    if (array[i] % 2 == 0)
+    {
+        console.write(array[i]);
+    }
+    i = i + 1;
+}
